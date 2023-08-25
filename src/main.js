aa6f28d88415809b10b4dc71daca58df43a05cce
silviazhao <silviazhao@tencent.com>
2023-08-25 17:50:28 +0800

    feat: update svg

--- src/main.js
+++ src/main.js
@@ -1,16 +1,21 @@
+const imageBaseUrl =
+  "https://github.com/Ashster/poppy-park/blob/main/assets/img/";
+const imageLists = ["maltese_1.png?raw=true", "maltese_2.png?raw=true"];
+
 const drawCanvas = (canvasID) => {
   let canvas = document.getElementById(canvasID);
   if (canvas.getContext) {
     var ctx = canvas.getContext("2d");
 
-    drawImage(ctx, "assets/img/maltese_1.png");
+    drawImage(ctx, imageBaseUrl + imageLists[0]);
   }
 };
 
 const drawImage = (ctx, src) => {
   let image = new Image();
+  imageBaseUrl.crossOrigin = "Anonymous";
   image.onload = () => {
-    ctx.drawImage(image, 0, 0);
+    ctx.drawImage(image, 0, 0, 10, 10);
   };
   image.src = src;
 };
