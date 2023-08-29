b333556faf06d4d4438ced84f8d44ad4ba87b579
silviazhao <silviazhao@tencent.com>
2023-08-29 10:17:24 +0800

    feat: draw image cross origin

--- src/main.js
+++ src/main.js
@@ -1,23 +1,30 @@
+const canvas = document.getElementById("poppy-container");
+canvas.width = window.innerWidth * 5;
+canvas.height = window.innerHeight * 5;
+const ctx = canvas.getContext("2d");
+
 const imageBaseUrl =
   "https://github.com/Ashster/poppy-park/blob/main/assets/img/";
-const imageLists = ["maltese_1.png?raw=true", "maltese_2.png?raw=true"];
+const imageLists = ["maltese_1.svg?raw=true", "maltese_2.svg?raw=true", "maltese_3.svg?raw=true", "maltese_4.svg?raw=true", "maltese_5.svg?raw=true", "maltese_6.svg?raw=true"];
+const imageMap = new Map();
 
-const drawCanvas = (canvasID) => {
-  let canvas = document.getElementById(canvasID);
-  if (canvas.getContext) {
-    var ctx = canvas.getContext("2d");
-
-    drawImage(ctx, imageBaseUrl + imageLists[0]);
-  }
-};
-
-const drawImage = (ctx, src) => {
-  let image = new Image();
+const drawImage = (src, x, y) => {
+  let image = imageMap.get(src) || new Image();
   imageBaseUrl.crossOrigin = "Anonymous";
   image.onload = () => {
-    ctx.drawImage(image, 0, 0, 10, 10);
+    ctx.drawImage(image, x, y, 300, 300);
   };
   image.src = src;
+  imageMap.set(src, image);
 };
 
-drawCanvas("poppy-container");
+const drawCanvas = () => {
+  // for (let i = 0; i < canvas.height; i = i + 320) {
+  for (let j = 0; j < canvas.width; j = j + 320) {
+    drawImage(imageBaseUrl + imageLists[(j / 320) % imageLists.length], 0, j)
+  }
+  // }
+  // drawImage(imageBaseUrl + imageLists[0]);
+}
+
+drawCanvas()
